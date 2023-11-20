651eec4fe0155c531adf7d7b0141523f6ffd0763
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2023-11-20 16:05:54 +0530

    feat : bind data from url

--- Async-redux.js
+++ Async-redux.js
@@ -1,8 +1,14 @@
-//create async actions in redux
-
-//Getting the data and fetch
 const redux = require("redux");
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
+//create async actions in redux
+//Getting the data and fetch
+//axios: request to an Api end point
+//redux-Thunk: Define async action creators
 
 //initial state
 const initialState = {
@@ -16,24 +22,25 @@ const initialState = {
 const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
 const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
 const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
 //action-creators
-function fetchUserRequest() {
+const fetchUserRequest = () => {
   return {
     type: FETCH_USERS_REQUESTED,
   };
-}
+};
 
-function fetchUserSuccess(users) {
+const fetchUserSucceed = (users) => {
   return {
     type: FETCH_USERS_SUCCEEDED,
     payload: users,
   };
-}
+};
 
-const fetchUserFaild = (err) => {
+const fetchUserFailed = (error) => {
   return {
     type: FETCH_USERS_FAILED,
-    payload: err,
+    payload: error,
   };
 };
 
@@ -44,16 +51,19 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: (state.loading = true),
+        error: "",
       };
     case FETCH_USERS_SUCCEEDED:
       return {
         ...state,
+        loading: false,
         users: (state.users = action.payload),
       };
 
     case FETCH_USERS_FAILED:
       return {
         ...state,
+        loading: false,
         error: (state.error = action.payload),
       };
 
@@ -62,11 +72,25 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const fetchUsers = () => {
+  //thunk helpe to return  function from action creators
+  return function (dispatch) {
+    dispatch(fetchUserRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUserSucceed(users));
+      })
+      .catch((error) => {
+        dispatch(fetchUserFailed(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 console.log("Initial state :", store.getState());
 
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state ", store.getState())
-);
-store.dispatch(fetchUserRequest());
+store.subscribe(() => console.log("updated state ", store.getState()));
+store.dispatch(fetchUsers());
