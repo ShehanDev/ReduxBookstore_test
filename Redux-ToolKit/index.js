d0595af3565088a9f4ceda9083b6f3e2d8d9e42f
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2023-11-23 18:42:14 +0530

    fix : fix the fetch users func

--- Redux-ToolKit/index.js
+++ Redux-ToolKit/index.js
@@ -1,8 +1,8 @@
 const store = require("./app/store");
 const { bookActions } = require("./fetures/books/bookSlice");
 const { magazineActions } = require("./fetures/magzines/magzineSlice");
-const { fetchUsers } = require("./fetures/user/userSlice").fetchUsers;
-console.log("Initial state :" + store.getState());
+const { fetchUsers } = require("./fetures/user/userSlice");
+console.log("Initial state :" + JSON.stringify(store.getState()));
 
 const unSubscribe = store.subscribe(() => {});
 
